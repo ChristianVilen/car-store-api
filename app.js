10b172c0f18abdd0271f1f5d073ace23db18028d
Christian Vilen <cvilen@phz.fi>
2021-03-13 11:06:40 +0200

    Deleted unnecessary package, added prettier settings json and formatted project

--- app.js
+++ app.js
@@ -10,20 +10,19 @@ const port = 8000;
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
 app.use("/cars", carRoute);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Connect to db
 mongoose
   .connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
+    useFindAndModify: false
   })
   .then(() => console.log("Linked 😉"))
   .catch(err => console.error(err));
